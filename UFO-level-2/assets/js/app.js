f2d40d18d03c33f3d946e99f8467b57cb340774b
relaernu <relaernu@gmail.com>
2021-03-16 09:27:09 +1100

    Level 2 finished

--- UFO-level-2/assets/js/app.js
+++ UFO-level-2/assets/js/app.js
@@ -49,7 +49,7 @@ document.getElementById(id_shape).innerHTML = createShapeSelList(events);
 
 // create shape select list
 function createShapeSelList(source) {
-    var sel = "<option>All</option>";
+    var sel = `<option value="">All</option>`;
     var shapes = [];
     source.forEach(function(ev) {
         if(!shapes.includes(ev.shape)) {
@@ -57,7 +57,7 @@ function createShapeSelList(source) {
         }
     });
     shapes.forEach(function(shape) {
-        sel += `<option>${shape}</option>`;
+        sel += `<option value="${shape}">${shape}</option>`;
     });
     return sel;
 }
@@ -68,9 +68,44 @@ var id_searchText = "searchText";
 var id_searchForm = "searchForm";
 var id_searchResult = "searchResult";
 document.getElementById(id_searchForm).addEventListener("submit", search);
+document.getElementById("startDate").addEventListener("change", datechange);
+document.getElementById("endDate").addEventListener("change", datechange);
+
+// to add event to start date and end date change
+// make sure start date <= end date
+function datechange(event) {
+    var s_date = document.getElementById("startDate").value;
+    var e_date = document.getElementById("endDate").value;
+    // trigger by changing start date
+    if(this.id === "startDate") {
+        // end date has been selected or not
+        if (e_date) {
+            // if end date has been selected, is it earlier than start date?
+            // if end date earlier than start date, make it the same as start date
+            if(e_date < s_date) {
+                document.getElementById("endDate").value = s_date;
+            }
+        }
+    }
+    // trigger by changing end date
+    else {
+        if (s_date) {
+            if(e_date < s_date) {
+                document.getElementById("startDate").value = e_date;
+            }
+        }
+    }
+}
 
 function createSearchText(date) {
     return `<h1>Search date: <span class="badge badge-info">${date}</span></h1>`;
+}
+
+function AusDateToStdDate(ausDate) {
+    var splitDate = ausDate.split("/");
+    return splitDate[2] + "-" + 
+        (splitDate[1].length == 1 ?  `0${splitDate[1]}` : splitDate[1]) + "-" +
+        (splitDate[0].length == 1 ?  `0${splitDate[0]}` : splitDate[0]);
 }
 
 function search(event) {
@@ -78,40 +113,44 @@ function search(event) {
 
     // get current input
     form = document.getElementById(id_searchForm);
-    var date = form.elements[0].value;
+    var startDate = form.elements["startDate"].value;
+    var endDate = form.elements["endDate"].value;
+    var cities = form.elements["cities"].value;
+    var states = form.elements["states"].value;
+    var shape = form.elements["shape"].value;
 
-    // if not dd/mm/yyyy format, 
-    var dateparts = date.split("/");
-    if(dateparts.length != 3) {
-        alert("Wrong date format, input as \"dd/mm/yyyy\".");
-        return;
-    }
-    var d = new Date(`${dateparts[2]}-${dateparts[1]}-${dateparts[0]}`);
-    if(isNaN(d)) {
-        alert("Wrong date format, input as \"dd/mm/yyyy\".");
-        return;
-    }
-    
-    // to get rid of the first 0 for day and month digit
-    str_date = (dateparts[0][0] == 0 ? dateparts[0][1] : dateparts[0]) + "/" +
-                (dateparts[1][0] == 0 ? dateparts[1][1] : dateparts[1]) + "/" +
-                dateparts[2];
+    var filterdata = events;
 
-    // show the search criteria
-    document.getElementById(id_searchText).innerHTML = createSearchText(str_date);
-
-    // get the filter data
-    filter_events = events.filter(ev => ev.datetime === str_date);
-
-    // // show the filter data
-    if(filter_events.length != 0) {
-        document.getElementById(id_searchResult).innerHTML = createTable(filter_events);
-    }
-    else {
-        document.getElementById(id_searchResult).innerHTML = "<h3>No result found on that day.";
+    // get datetime later than start date
+    if(startDate.length>0) {
+        filterdata = filterdata.filter(x=>AusDateToStdDate(x.datetime) >= startDate);
     }
 
-    // scroll to search section
-    document.getElementById(id_searchSection).scrollIntoView();
+    // get datetime earlier than end date
+    if(endDate.length>0) {
+        filterdata = filterdata.filter(x=>AusDateToStdDate(x.datetime) <= endDate);
+    }
 
+    // filter states
+    if (states.length > 0) {
+        var state = states.split(",");
+        var new_filter = [];
+        state.forEach(x => new_filter = new_filter.concat(filterdata.filter(y => y.state == x)));
+        filterdata = new_filter;
+    }
+
+    // filter cities
+    if(cities.length > 0) {
+        var city = cities.split(",");
+        var new_filter = [];
+        city.forEach(x => new_filter = new_filter.concat(filterdata.filter(y => y.city == x)));
+        filterdata = new_filter;
+    }
+
+    // filter shapes
+    if(shape.length > 0) {
+        filterdata = filterdata.filter(x => x.shape === shape);
+    }
+
+    document.getElementById(id_searchResult).innerHTML = createTable(filterdata);
 }
